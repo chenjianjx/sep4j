89a613fd41a010482ca124c3f470ded911f913bc
chenjianjx <chenjianjx@gmail.com>
2014-11-29 21:26:27 +0800

    the first show case

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-sep4j
-=====
+#sep4j
 
-sep4j = Simplified Excel Processing for Java, with which you can read or write an excel file with a simple method call. 
+sep4j = Simplified Excel Processing for Java, with which you can write or read an excel file with a simple method call.
+
+##Basic Examples
+
+###Save(Write)
+```java
+
+		Collection<User> users = Arrays.asList(user1, user2);
+		LinkedHashMap<String, String> headerMap = new LinkedHashMap<String, String>();
+		headerMap.put("userId", "User Id");  //userId is a property of User class
+		headerMap.put("firstName", "First Name");
+		headerMap.put("lastName", "Last Name");
+		
+		ExcelUtils.save(headerMap, users, outputStream);
+
+```
+
+
+
+###Parse(Read)
+ 
--- src/test/java/org/sep4j/showcase/SepShowcases.java
+++ src/test/java/org/sep4j/showcase/SepShowcases.java
@@ -1,5 +1,99 @@
 package org.sep4j.showcase;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+import org.apache.commons.io.FileUtils;
+import org.sep4j.ExcelUtils;
+
+/**
+ * for documentation only
+ * 
+ * @author chenjianjx
+ */
 public class SepShowcases {
+
+	public static void main(String[] args) throws IOException {
+		basicSave();
+	}
+
+	private static void basicSave() throws IOException {
+
+
+
+		User user1 = new User();
+		user1.setUserId(1);
+		user1.setFirstName("Lei");
+		user1.setLastName("Li");
+		
+		
+		
+		User user2 = new User();
+		user2.setUserId(2);
+		user2.setFirstName("Jim");
+		user2.setLastName("Green");
+
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		
+		Collection<User> users = Arrays.asList(user1, user2);
+		LinkedHashMap<String, String> headerMap = new LinkedHashMap<String, String>();
+		headerMap.put("userId", "User Id");  //userId is the property of User class
+		headerMap.put("firstName", "First Name");
+		headerMap.put("lastName", "Last Name");
+		
+		ExcelUtils.save(headerMap, users, outputStream);
+		
+		
+		
+		
+		byte[] excel = outputStream.toByteArray();
+		FileUtils.writeByteArrayToFile(createFile("save"), excel);
+
+	}
+
+	private static File createFile(String prefix) {
+		File dir = new File(System.getProperty("user.home"), "/temp/sep");
+		dir.mkdirs();
+		String filename = prefix + System.currentTimeMillis() + ".xlsx";
+		File file = new File(dir, filename);
+		return file;
+	}
+
+	private static final class User {
+
+		private long userId;
+		private String firstName;
+		private String lastName;
+
+		public long getUserId() {
+			return userId;
+		}
+
+		public void setUserId(long userId) {
+			this.userId = userId;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+	}
 
 }
